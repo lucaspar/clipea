128e46fb016ccea31a3a5683c348d8bdde07106d
Lucas Parzianello <lucaspar@users.noreply.github.com>
2025-10-29 10:25:53 -0400

    could not debug; restoring log level to info

--- clipea/clipea_llm.py
+++ clipea/clipea_llm.py
@@ -20,11 +20,9 @@ def init_llm(llm_model: str = "") -> llm.Model:
     Returns:
         llm.Model
     """
-    log.debug(f"Initializing llm with model: {llm_model}")
     model = llm.get_model(llm_model)
 
     if model.needs_key:
-        log.debug(f"getting key for model.needs_key: {model.needs_key}")
         model.key = llm.get_key(
             explicit_key="", key_alias=model.needs_key, env_var=model.key_env_var
         )
